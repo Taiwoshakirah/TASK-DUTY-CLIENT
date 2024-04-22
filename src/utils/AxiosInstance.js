07f0a8b37e6b1a49c0909bf3325d67f279dcf088
Taiwoshakirah <shakirattaiwo6@gmail.com>
2024-04-22 14:25:08 -0700

    changes

--- src/utils/AxiosInstance.js
+++ src/utils/AxiosInstance.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({ baseURL: "http://localhost:3000" });
+// const axiosInstance = axios.create({ baseURL: "http://localhost:3000" });
+const axiosInstance = axios.create({baseURL:"https://task-duty-server-52k4.onrender.com"})
 
 export default axiosInstance;
